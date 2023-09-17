6c17c233098d224e4f2922f5b6e4f22e328bab7c
FeuSalamander <ferreresarnaud@gmail.com>
2023-09-17 20:20:10 +0200

    Initial commit

---
+++ src/main/java/feusalamander/cs_mo/CS_MO.java
@@ -0,0 +1,17 @@
+package feusalamander.cs_mo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CS_MO extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
